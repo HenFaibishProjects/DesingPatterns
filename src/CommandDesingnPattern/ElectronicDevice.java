54bc7a7c20e5334ec4ce765cf5d6570877cc8f2d
Henf <bandel79@gmail.com>
2016-11-29 11:05:50 +0200

    ddfs

---
+++ src/CommandDesingnPattern/ElectronicDevice.java
@@ -0,0 +1,12 @@
+package CommandDesingnPattern;
+public interface ElectronicDevice {
+	
+	public void on();
+	
+	public void off();
+	
+	public void volumeUp();
+	
+	public void volumenDown();
+	
+}
---
+++ src/FactoryMethod/RocketEnemyShip.java
@@ -0,0 +1,13 @@
+package FactoryMethod;
+
+public class RocketEnemyShip extends EnemyShip {
+	
+	public RocketEnemyShip(){
+		
+		setName("Rocket Enemy Ship");
+		
+		setDamage(10.0);
+		
+	}
+	
+}
